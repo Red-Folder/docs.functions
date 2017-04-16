7691e61009f9174367bd28545b090b0868c17ed5
Red-Folder <markbryantaylor@gmail.com>
2017-04-16 20:29:35 +0100

    Added FtpsClient

---
+++ src/DocFunctions.Lib/Clients/FtpsClient.cs
@@ -0,0 +1,46 @@
+﻿using DocFunctions.Lib.Wappers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocFunctions.Lib.Clients
+{
+    public class FtpsClient : IFtpsClient
+    {
+        private string _host;
+        private string _username;
+        private string _password;
+
+        public FtpsClient(string host, string username, string password)
+        {
+            _host = host;
+            _username = username;
+            _password = password;
+        }
+
+        public void Upload(string filename, string contents)
+        {
+            string fullname = String.Format("ftp://{0}/{1}", _host, filename);
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(fullname);
+            request.Credentials = new NetworkCredential(_username, _password);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.EnableSsl = true;
+            request.UseBinary = true;
+
+            var payload = Encoding.UTF8.GetBytes(contents);
+            request.ContentLength = payload.Length;
+            using (Stream s = request.GetRequestStream())
+            {
+                s.Write(payload, 0, payload.Length);
+            }
+
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            response.Close();
+        }
+    }
+}
---
+++ tests/DocFunctions.Lib.Integration/Clients/FtpsClientTests.cs
@@ -0,0 +1,30 @@
+﻿using DocFunctions.Lib.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Common.Helpers;
+using Xunit;
+
+namespace DocFunctions.Lib.Integration.Clients
+{
+    public class FtpsClientTests
+    {
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void UploadFile()
+        {
+            var host = AppSettings.AppSetting("ftps-host");
+            var username = AppSettings.AppSetting("ftps-username");
+            var password = AppSettings.AppSetting("ftps-password");
+
+            var filename = $"/site/contentroot/test-{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}.html";
+            var contents = "<html><body>Hello World</body></html>";
+
+            var sut = new FtpsClient(host, username, password);
+
+            sut.Upload(filename, contents);
+        }
+    }
+}
