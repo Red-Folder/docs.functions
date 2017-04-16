49445c7c075373f5a50c6507dbde7bcb981cecf7
Red-Folder <markbryantaylor@gmail.com>
2017-04-16 16:16:19 +0100

    Added GithubClient

--- src/DocFunctions.Lib/Clients/GithubClient.cs
+++ src/DocFunctions.Lib/Clients/GithubClient.cs
@@ -9,9 +9,34 @@ namespace DocFunctions.Lib.Clients
 {
     public class GithubClient : IGithubReader
     {
+        private string _username;
+        private string _key;
+        private string _repo;
+
+        public GithubClient(string username, string key, string repo)
+        {
+            _username = username;
+            _key = key;
+            _repo = repo;
+        }
+
         public string GetRawFile(string path)
         {
-            throw new NotImplementedException();
+            var client = GetClient();
+
+            var contents = client.Repository.Content.GetAllContents("red-folder", "red-folder.docs.staging", path).Result;
+
+            return contents.First().Content;
+        }
+
+        private Octokit.GitHubClient GetClient()
+        {
+            var credentials = new Octokit.Credentials(_username, _key);
+            var connection = new Octokit.Connection(new Octokit.ProductHeaderValue("Red-Folder.DocFunctions"))
+            {
+                Credentials = credentials
+            };
+            return new Octokit.GitHubClient(connection);
         }
     }
 }
---
+++ tests/DocFunctions.Lib.Integration/Clients/GithubClientTests.cs
@@ -0,0 +1,29 @@
+﻿using DocFunctions.Lib.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Common.Helpers;
+using Xunit;
+
+namespace DocFunctions.Lib.Integration.Clients
+{
+    public class GithubClientTests
+    {
+        [Fact]
+        [Trait("Category", "Integration")]
+        public void GetsRawData()
+        {
+            var username = AppSettings.AppSetting("github-username");
+            var key = AppSettings.AppSetting("github-key");
+            var repo = AppSettings.AppSetting("github-repo");
+
+            var sut = new GithubClient(username, key, repo);
+
+            var result = sut.GetRawFile("README.md");
+
+            Assert.Contains("A test version of my red-folder.docs repo - used for Staging testing", result);
+        }
+    }
+}
