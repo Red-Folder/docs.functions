545d817d8fb035d9556c1da6e4b4c5383ddb2351
Red-Folder <markbryantaylor@gmail.com>
2017-04-27 21:23:23 +0100

    Refactor of GitHub

--- tests/DocFunctions.Integration/Helpers/GitHub.cs
+++ tests/DocFunctions.Integration/Helpers/GitHub.cs
@@ -27,72 +27,83 @@ namespace DocFunctions.Integration.Helpers
 
         public async Task CreateTestBogAsync()
         {
+            var github = CreateClient();
+
+            var parent = await GetParent(github);
+            var latestCommit = await GetLatestCommit(github, parent.Object.Sha);
+            var imgBlogRef = await GetImageBlogReference(github, @"Assets\Image.png");
+            var metaBlogRef = await GetMetaBlogReference(github, @"Assets\blog.json");
+            var mdBlogRef = await GetMarkdownBlogReference(github, @"Assets\blog.md");
+            var newTree = await CreateCommitTree(github, latestCommit, imgBlogRef, metaBlogRef, mdBlogRef);
+            var commit = await CreateCommit(github, newTree.Sha, parent.Object.Sha);
+
+            await AttachCommitToParent(github, commit.Sha);
+        }
+
+        private Octokit.GitHubClient CreateClient()
+        {
             var credentials = new Octokit.Credentials(_username, _key);
             var connection = new Octokit.Connection(new Octokit.ProductHeaderValue("Red-Folder.DocFunctions.IntegrationTests"))
             {
                 Credentials = credentials
             };
-            var github = new Octokit.GitHubClient(connection);
+            return new Octokit.GitHubClient(connection);
+        }
 
-            // Below adapted from http://laedit.net/2016/11/12/GitHub-commit-with-Octokit-net.html
+        private Task<Reference> GetParent(Octokit.GitHubClient github)
+        {
+            return github.Git.Reference.Get(_username, _repo, "heads/master");
+        }
 
-            //-------------------------------------------------------------------------------------------
-            // Get the latest commit
-            //-------------------------------------------------------------------------------------------
-            // Get reference of master branch
-            var masterReference = await github.Git.Reference.Get(_username, _repo, "heads/master");
-            // Get the laster commit of this branch
-            var latestCommit = await github.Git.Commit.Get(_username, _repo, masterReference.Object.Sha);
+        private Task<Commit> GetLatestCommit(Octokit.GitHubClient github, string parentSha)
+        {
+            return github.Git.Commit.Get(_username, _repo, parentSha);
+        }
 
-            //-------------------------------------------------------------------------------------------
-            // Create an image
-            //-------------------------------------------------------------------------------------------
-            // For image, get image content and convert it to base64
-            var imgBase64 = Convert.ToBase64String(File.ReadAllBytes(@"Assets\Image.png"));
+        private Task<BlobReference> GetImageBlogReference(Octokit.GitHubClient github, string filename)
+        {
+            var imgBase64 = Convert.ToBase64String(File.ReadAllBytes(filename));
             // Create image blob
             var imgBlob = new NewBlob { Encoding = EncodingType.Base64, Content = (imgBase64) };
-            var imgBlobRef = await github.Git.Blob.Create(_username, _repo, imgBlob);
+            return github.Git.Blob.Create(_username, _repo, imgBlob);
+        }
 
-            //-------------------------------------------------------------------------------------------
-            // Create a meta file
-            //-------------------------------------------------------------------------------------------
-            // Create text blob
-            var metaContents = File.ReadAllText(@"Assets\blog.json").Replace("[BLOGNAME]", _blogname);
+        private Task<BlobReference> GetMetaBlogReference(Octokit.GitHubClient github, string filename)
+        {
+
+            var metaContents = File.ReadAllText(filename).Replace("[BLOGNAME]", _blogname);
             var metaBlob = new NewBlob { Encoding = EncodingType.Utf8, Content = metaContents };
-            var metaBlobRef = await github.Git.Blob.Create(_username, _repo, metaBlob);
+            return github.Git.Blob.Create(_username, _repo, metaBlob);
+        }
 
-            //-------------------------------------------------------------------------------------------
-            // Create a markdown file
-            //-------------------------------------------------------------------------------------------
-            // Create text blob
-            var mdContents = File.ReadAllText(@"Assets\blog.md").Replace("[BLOGNAME]", _blogname);
+        private Task<BlobReference> GetMarkdownBlogReference(Octokit.GitHubClient github, string filename)
+        {
+            var mdContents = File.ReadAllText(filename).Replace("[BLOGNAME]", _blogname);
             var mdBlob = new NewBlob { Encoding = EncodingType.Utf8, Content = mdContents };
-            var mdBlobRef = await github.Git.Blob.Create(_username, _repo, mdBlob);
+            return github.Git.Blob.Create(_username, _repo, mdBlob);
+        }
 
-            //-------------------------------------------------------------------------------------------
-            // Create a tree
-            //-------------------------------------------------------------------------------------------
-            // Create new Tree
+        private Task<TreeResponse> CreateCommitTree(Octokit.GitHubClient github, Commit latestCommit, BlobReference imgBlobRef, BlobReference metaBlobRef, BlobReference mdBlobRef)
+        {
             var nt = new NewTree { BaseTree = latestCommit.Tree.Sha };
+            
             // Add items based on blobs
             nt.Tree.Add(new NewTreeItem { Path = $"{_blogname}/Image.jpg", Mode = "100644", Type = TreeType.Blob, Sha = imgBlobRef.Sha });
             nt.Tree.Add(new NewTreeItem { Path = $"{_blogname}/blog.json", Mode = "100644", Type = TreeType.Blob, Sha = metaBlobRef.Sha });
             nt.Tree.Add(new NewTreeItem { Path = $"{_blogname}/blog.md", Mode = "100644", Type = TreeType.Blob, Sha = mdBlobRef.Sha });
 
-            var newTree = await github.Git.Tree.Create(_username, _repo, nt);
+            return github.Git.Tree.Create(_username, _repo, nt);
+        }
 
-            //-------------------------------------------------------------------------------------------
-            // Create commit
-            //-------------------------------------------------------------------------------------------
-            // Create Commit
-            var newCommit = new NewCommit($"Created test blog {_blogname}", newTree.Sha, masterReference.Object.Sha);
-            var commit = await github.Git.Commit.Create(_username, _repo, newCommit);
+        private Task<Commit> CreateCommit(Octokit.GitHubClient github, string newTreeSha, string parentSha)
+        {
+            var newCommit = new NewCommit($"Created test blog {_blogname}", newTreeSha, parentSha);
+            return github.Git.Commit.Create(_username, _repo, newCommit);
+        }
 
-            //-------------------------------------------------------------------------------------------
-            // And push it
-            //-------------------------------------------------------------------------------------------
-            // Update HEAD with the commit
-            await github.Git.Reference.Update(_username, _repo, "heads/master", new ReferenceUpdate(commit.Sha));
+        private Task<Reference> AttachCommitToParent(Octokit.GitHubClient github, string commitSha)
+        {
+            return github.Git.Reference.Update(_username, _repo, "heads/master", new ReferenceUpdate(commitSha));
         }
     }
 }
