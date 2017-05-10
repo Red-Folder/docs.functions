b8d743a0a35ff29c129c0a45a13bf062bdde08b7
Mark Taylor <markbryantaylor@gmail.com>
2017-05-10 20:28:56 +0100

    Partial convert to web app version

---
+++ src/DocFunctions/Blog/run.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Configuration;
+using DocFunctions.Lib.Clients;
+using DocFunctions.Lib.Utils;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs.Host;
+using System;
+
+namespace DocFunctions.Functions
+{
+    public class Blog
+    {
+        public static HttpResponseMessage Run(HttpRequestMessage req, string blogUrl, TraceWriter log)
+        {
+            log.Info("Blog Request initiated");
+
+            try
+            {
+
+                // Get all settings
+                var appInsightsKey = ConfigurationManager.AppSettings["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                var blogMetaContainerName = ConfigurationManager.AppSettings["BlogMetaStorageContainerName"];
+                var blogMetaConnectionString = ConfigurationManager.ConnectionStrings["BlogMetaStorage"].ConnectionString;
+
+                // Setup objects
+                ILogger logger = new NullLogger();
+                if (appInsightsKey != null && appInsightsKey.Length > 0)
+                {
+                    logger = new ApplicationInsightsLogger(appInsightsKey);
+                }
+
+                var blogMetaRepository = new BlogMetaRepository(blogMetaConnectionString, blogMetaContainerName);
+
+                HttpResponseMessage response = null;
+                if (blogUrl == "*")
+                {
+                    return blogMetaRepository.Get().HasValue ? req.CreateResponse(HttpStatusCode.OK, blogMetaRepository.Get().Value) : req.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return blogMetaRepository.Get(blogUrl).HasValue ? req.CreateResponse(HttpStatusCode.OK, blogMetaRepository.Get(blogUrl).Value) : req.CreateResponse(HttpStatusCode.NotFound);
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error("Function failed", ex);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ src/DocFunctions/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DocFunctions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DocFunctions")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dfe7d80e-1a88-48ff-8b9c-6d29dc7c9269")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
